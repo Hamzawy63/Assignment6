4791c697504d8922c699d500c7bb2eb59e4b29b2
Hamzawy63 <hamzahassan835@gmail.com>
2019-05-03 13:40:13 +0200

    add a method to test circular array performence

--- Tests/eg/edu/alexu/csd/datastructure/queue/cs26/arrQueueTest.java
+++ Tests/eg/edu/alexu/csd/datastructure/queue/cs26/arrQueueTest.java
@@ -12,9 +12,8 @@ public class arrQueueTest {
         assertTrue(q.isEmpty());
         try {
             assertEquals(10, q.dequeue());
-        }catch (Exception e)
-        {
-            assertEquals("Queue is Empty",e.getMessage());  // give you exception when u use dequeue with empty exception
+        } catch (Exception e) {
+            assertEquals("Queue is Empty", e.getMessage());  // give you exception when u use dequeue with empty exception
         }
         q.enqueue(10);
         assertEquals(1, q.size());
@@ -26,9 +25,8 @@ public class arrQueueTest {
         assertEquals(5, q.size());
         try {
             q.enqueue(10);
-        } catch (Exception e)
-        {
-           assertEquals("Queue is full",e.getMessage()); // test that you can't enter extra elements
+        } catch (Exception e) {
+            assertEquals("Queue is full", e.getMessage()); // test that you can't enter extra elements
         }
         assertEquals(10, q.dequeue());
         assertEquals(11, q.dequeue());
@@ -36,22 +34,37 @@ public class arrQueueTest {
         assertEquals(13, q.dequeue());
         assertEquals('A', q.dequeue());
         assertTrue(q.isEmpty());  // check if the queue is empty
-          try {
-              assertEquals(10, q.dequeue());
-          }catch (Exception e)
-          {
-              assertEquals("Queue is Empty",e.getMessage());  // give you exception when u use dequeue with empty exception
-          }
+        try {
+            assertEquals(10, q.dequeue());
+        } catch (Exception e) {
+            assertEquals("Queue is Empty", e.getMessage());  // give you exception when u use dequeue with empty exception
+        }
     }
-    @Test(expected=IllegalStateException.class)
-    public void testIllegalStateException()  {
+
+    @Test(expected = IllegalStateException.class)
+    public void testIllegalStateException() {
         arrQueue q = new arrQueue(2);
         q.enqueue(11);
         q.enqueue(12);
         q.enqueue(12);
     }
+    @Test
+    public void testCicular()
+    {
+        arrQueue q = new arrQueue(4);
+        q.enqueue(11);
+        q.enqueue(12);
+        q.enqueue(13);
+        q.enqueue(14);
+        q.dequeue();
+        q.dequeue();
+        q.enqueue(11);
+        q.enqueue(11);
+        assertEquals(4, q.size());
 
 
+
+    }
 
 
 }
